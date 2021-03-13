a56f27d68c835774351ceb6c1c72d2f3fde8a0ee
ChoiJunho <ajchoi0928@gmail.com>
2021-03-14 02:15:47 +0900

    Algorithm_assignment Week 1

---
+++ src/lecture/trying/week1/Assignment1.java
@@ -0,0 +1,40 @@
+package lecture.trying.week1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+*  2017112095 컴퓨터공학과 최준호
+*  누적 평균을 구하는 프로그램
+*
+* */
+public class Assignment1  {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in); // Stream 을 이용하면 실행시간을 단축시킬수있다
+        Assignment1 assign = new Assignment1();
+
+        String input="";
+
+        System.out.print("배열을 입력해주세요 :");
+        input = sc.nextLine(); // 문자열 전체를 받음
+
+        assign.solution(input);
+
+    }
+
+    public void solution(String str)  {
+       /* ArrayList<Integer> arr = new ArrayList<>();*/
+        String[] temp;
+        int sum=0;  // X[0] + X[1] +...X[i] 의 합을 저장하는 변수
+        double result=0.0; // 결과값을 출력할 변수
+        /*int result=0; // 출력할 결과값*/
+        temp=str.split(" "); // 문자열에서 공백을 기준으로 분리하여 String 배열에 넣어줌 입력받은 문자열의 숫자의 갯수에 맞춰 생성될것
+
+        System.out.println("Avr 출력");
+        for(int i =0; i<temp.length; i++){
+            sum += Integer.parseInt(temp[i]);
+            result = sum / (i+1.0);
+            System.out.println("Avr["+i+"]: " + result);
+        }
+    }
+}
---
+++ src/lecture/trying/week1/Assignment2_2.java
@@ -0,0 +1,46 @@
+package lecture.trying.week1;
+
+import java.util.Scanner;
+
+public class Assignment2_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int M;
+        String input;
+        System.out.println("2017112095 컴퓨터공학과 최준호 문제2-2");
+        System.out.println("M과 c를 입력해주세요. 입력 예 M (c0,c1,c2,...,cd-1) ");
+        //M과 c=(c0,c1,c2,c3,...cd) 입력받음
+        M=sc.nextInt();
+        sc.nextLine(); // 한번 버퍼를 비워줌
+
+        input = sc.nextLine();
+        input = input.substring(input.indexOf("(")+1, input.indexOf(")")); // 이후 괄호 분리됨
+
+
+    }
+    public int solution(int M, String input){
+        int[] c = convertInt(input.split(",")); // 동전 화폐단위 문자열 배열을 int형 배열로 변환해줌
+        int d= c.length;
+        int[] k = new int[d];
+
+        int smallestNumberOfCoins = Integer.MAX_VALUE; // int형의 최대값을 넣어줌
+        int valueOfCoins=0;
+
+        /*for(int i=0; i<d; i++){ // for each ( k1,k2,...,kd )
+            for(int j=0; j<d; j++){
+                k[i]=M/Integer.parseInt(c[j]);
+                M=M-Integer.parseInt(c[j])*k[j];
+            }
+        }*/
+
+    }
+
+    /* 매번 Integer.parseInt 호출하면 성능이 안좋을것 같아서 문자열 배열 c를 한번에 int[] 로 바꿔놓기 위한 스태틱 메소드 */
+    public static int[] convertInt(String[] str){
+        int[] result = new int[str.length];
+        for(int i=0; i<str.length; i++){
+            result[i]=Integer.parseInt(str[i]);
+        }
+    }
+}
