a244260087abc1b11638d74395478a5dee0d6cac
ChoiJunho <ajchoi0928@gmail.com>
2021-09-18 01:13:25 +0900

    solving Kakao Internship problem, 숫자 문자열과 영단어

---
+++ src/programmers/kakao/internship/yr2020/StringWord.java
@@ -0,0 +1,75 @@
+package programmers.kakao.internship.yr2020;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+/**
+ * 2021 카카오 채용 연계형 인턴쉽
+ * 숫자 문자열과 영단어 1차 풀이 0918
+ */
+public class StringWord {
+
+    public static HashMap<Integer, String> map = new HashMap<>() {{
+        put(0, "zero");
+        put(1, "one");
+        put(2, "two");
+        put(3, "three");
+        put(4, "four");
+        put(5, "five");
+        put(6, "six");
+        put(7, "seven");
+        put(8, "eight");
+        put(9, "nine");
+    }};
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        solution(input);
+        System.out.println(sb.toString());
+    }
+
+    public static void solution(String s) {
+        /*StringTokenizer st = new StringTokenizer(s); 구분자가 없어 아니라 이 방법 패스*/
+        String cursor = "";
+        String number = "";
+        int i = 0;
+        while (i < s.length()) {
+            cursor = s.substring(i, i + 1);
+            i++;
+            if (isNumber(cursor)) {
+                sb.append(cursor);
+            } else {
+                number += cursor;
+            }
+
+            if(map.containsValue(number)){
+                sb.append(getKey(map, number));
+                number = "";
+            }
+        }
+
+    }
+
+    public static boolean isNumber(String s) {
+        try {
+            return Integer.parseInt(s) >= 0 && Integer.parseInt(s) < 10;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static <K, V> K getKey(Map<K, V> map, V value) {
+        for (K key : map.keySet()) {
+            if (value.equals(map.get(key))) {
+                return key;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/programmers/kakao/internship/yr2020/StringWord2.java
@@ -0,0 +1,26 @@
+package programmers.kakao.internship.yr2020;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 2021 카카오 채용 연계형 인턴쉽
+ * 2차 풀이
+ */
+public class StringWord2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        StringWord2 sw2 = new StringWord2();
+        System.out.println(sw2.solution(s));
+    }
+    public int solution(String s){
+        String[] str = {"zero", "one", "two", "three", "four","five", "six", "seven", "eight", "nine"};
+        for(int i =0; i< str.length; i++){
+            s = s.replace(str[i],Integer.toString(i));
+            /*replaceAll 에선 정규식을 target 의 인자로 받을 수 있다. */
+        }
+        return Integer.parseInt(s);
+    }
+}
