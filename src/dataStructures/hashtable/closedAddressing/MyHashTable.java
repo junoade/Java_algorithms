a27b0b0cde254203431d34f1fa02af0639564bf2
Junho Choi <ajchoi0928@gmail.com>
2022-08-21 21:42:50 +0900

    feat: 해시 테이블 - Closed Addressing, 체이닝 기법 간단하게 구현

--- src/dataStructures/hashtable/closedAddressing/MyHashTable.java
+++ src/dataStructures/hashtable/closedAddressing/MyHashTable.java
@@ -6,31 +6,73 @@ import dataStructures.hashtable.HashTable;
  * Closed Addressing, 체이닝 연습
  */
 public class MyHashTable implements HashTable {
+    Slot[] hashTable;
 
+    static class Slot {
+        String key;
+        String value;
+        Slot next;
+
+        Slot(String key, String value) {
+            this.key = key;
+            this.value = value;
+            next = null;
+        }
+    }
+
+    MyHashTable(int initialSize) {
+        hashTable = new Slot[initialSize];
+    }
 
     @Override
     public int hashFunc(String key) {
-        return 0;
+        return (int) (key.charAt(0)) % this.hashTable.length;
     }
 
     /**
      * Chaining 방식을 활용해서 해시테이블에 저장하기
+     *
      * @param key
      * @param value
      * @return
      */
     @Override
     public boolean saveData(String key, String value) {
-        return false;
+        int address = hashFunc(key);
+
+        /* 이미 동일한 key에 대한 슬롯이 있는 경우 */
+        if (this.hashTable[address] != null) {
+            Slot cursor = this.hashTable[address];
+            while (cursor.next != null) {
+                cursor = cursor.next;
+            }
+            cursor.next = new Slot(key, value);
+
+        } else {
+            this.hashTable[address] = new Slot(key, value);
+        }
+
+        return true;
     }
 
     /**
      * Chaining 방식을 고려하여, Key가 갖는 대한 링크드 리스트 순회 후 반환하기
+     *
      * @param key
      * @return
      */
     @Override
     public String getData(String key) {
-        return null;
+        int address = this.hashFunc(key);
+        StringBuilder sb = new StringBuilder();
+
+        if (this.hashTable[address] != null) {
+            Slot cursor = this.hashTable[address];
+            do {
+                sb.append(cursor.value).append(" ");
+                cursor = cursor.next;
+            } while (cursor != null);
+        }
+        return sb.toString();
     }
 }
--- src/dataStructures/hashtable/closedAddressing/MyHashTableTest.java
+++ src/dataStructures/hashtable/closedAddressing/MyHashTableTest.java
@@ -1,4 +1,28 @@
 package dataStructures.hashtable.closedAddressing;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MyHashTableTest {
+    MyHashTable hashTable;
+
+    @Before
+    public void init(){
+        hashTable = new MyHashTable(20);
+    }
+
+
+    @Test
+    public void testHashCollision(){
+        hashTable.saveData("J", "Junho Choi");
+        hashTable.saveData("J", "James Choi");
+        hashTable.saveData("J", "Jenny Choi");
+        hashTable.saveData("H", "Henry Choi");
+        hashTable.saveData("D", "Diana Choi");
+        System.out.println(hashTable.getData("J"));
+        assertEquals("Jenny Choi James Choi Jenny Choi ", hashTable.getData("J"));
+    }
+
 }
