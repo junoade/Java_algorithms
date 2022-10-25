31e626db61274dc24f62bcaef6a43dde80767b3e
Junho Choi <ajchoi0928@gmail.com>
2022-10-25 13:43:39 +0900

    solve : 프로그래머스 캐시 LRU 풀이 - 교체 알고리즘, Least Recent Used vs Least Frequency Used

---
+++ src/category/simulation/programmers/CacheHitLRU_Failed.java
@@ -0,0 +1,70 @@
+package category.simulation.programmers;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * LRU 와 LFU 헷갈리지 말자
+ */
+public class CacheHitLRU_Failed {
+
+    static int least_idx = 0;
+
+    static class Entry {
+        String value;
+
+        // int hit;
+        Entry(String value) {
+            this.value = value;
+        }
+    }
+
+    // O(N)
+    // 가장 오래된 idx(제일 왼쪽)을 반환
+    public void updateLRU(Queue<Entry> caches, String city, int cacheSize) {
+        if (caches.size() != cacheSize) {
+            caches.add(new Entry(city.toUpperCase()));
+        } else {
+            caches.poll();
+            caches.add(new Entry(city.toUpperCase()));
+        }
+    }
+
+    // O(N)
+    public Entry containsCity(Queue<Entry> caches, String city) {
+        Entry entry = null;
+        for (Entry e : caches) {
+            if (e.value.equals(city.toUpperCase())) {
+                entry = e;
+            }
+        }
+        return entry;
+    }
+
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        Queue<Entry> caches = new LinkedList<>();
+
+        if (cacheSize == 0) // 빼먹었다.
+            return cities.length * 5;
+
+        for (String city : cities) {
+            Entry findEntry = containsCity(caches, city);
+            if (findEntry != null) {
+                answer += 1;
+            } else {
+                updateLRU(caches, city, cacheSize);
+                answer += 5;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        CacheHitLRU_Failed c = new CacheHitLRU_Failed();
+        int cacheSize = 3;
+        String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+        System.out.println(c.solution(cacheSize, cities));
+    }
+}
---
+++ src/category/simulation/programmers/CacheHitLRU_Sol.java
@@ -0,0 +1,42 @@
+package category.simulation.programmers;
+
+import java.util.*;
+
+/**
+ * LRU 알고리즘
+ *  - 큐 자료구조를 쓰면 편하다.
+ *  1. 이미 참조한 자료(캐시에 저장된 자료)를 참조하는 경우, 해당 자료를 삭제하고 다시 최근에 참조했다고 옮긴다.
+ *  2. cache 사이즈가 꽉찬 경우, 제일 오래된 자료(head)를 poll(null 반환 가능성), remove(NoSuchElementException 발생 가능성), dequeue 하고,
+ *     city를 tail에 추가한다.
+ */
+public class CacheHitLRU_Sol {
+    public <T> void updateLRU(Queue<T> caches, T city, int cacheSize) {
+        if (caches.size() >= cacheSize) {
+            caches.poll();
+        }
+        caches.add(city);
+    }
+
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        Queue<String> caches = new LinkedList<>();
+
+        if (cacheSize == 0) // 빼먹었다.
+            return cities.length * 5;
+
+        for (String city : cities) {
+            if (caches.contains(city.toUpperCase())) { // O(N)
+                // 큐에서 해당 city를 제거한 다음 tail에 추가한다.
+                caches.remove(city.toUpperCase());
+                caches.add(city.toUpperCase());
+                answer += 1;
+            } else {
+                updateLRU(caches, city.toUpperCase(), cacheSize);
+                answer += 5;
+            }
+        }
+
+        return answer;
+    }
+
+}
