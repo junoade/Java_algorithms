11d5a7603c538c61dc85e6f62840518b24d40fea
Junho Choi <ajchoi0928@gmail.com>
2023-03-12 23:36:18 +0900

    feat : 프로그래머스 LV2 호텔 대실
    - 기존 풀이 문제점
    `Schedule.updateEndTime()` : "HH:MM"으로 문자열 비교하기 위해 청소 시간을 더하고 다시 예약 시간이 끝나는 시간을 업데이트 하는 과정에서 조건식 문제가 있었음
     - strHour 추가하고, 정수 값이 10 미만일 때 "01", "02" 처럼 되도록 변경
     - static 메소드로 바꿔서 빼냈음
    
    `solution()` : if-else 구문으로 배정된 방이 없는 초기상태에 대해 체크했었는데 어짜피 rooms가 null이면 반복이 되지 않으므로 불필요한 인덴트라고 생각하여 생략함
    
    `sortByTerms()` : 정렬 방식을 기존에는 방 점유 시간이 긴 순서대로 내림차순 정렬했었는데, 예약 시간이 빠른 순서로 정렬함

--- src/category/simulation/P_FindHotelRoom_F.java
+++ src/category/simulation/P_FindHotelRoom_F.java
@@ -4,25 +4,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class P_FindHotelRoom_F {
+    static final int CLEANING_MINUTE = 10;
+
     class Schedule {
         String startTime;
         String endTime;
-        final int CLEANING_MINUTE = 10;
 
         Schedule(String startTime, String endTime) {
             this.startTime = startTime;
             this.endTime = updateEndTime(endTime);
-        }
-
-        String updateEndTime(String endTime) {
-            int hour = Integer.parseInt(endTime.substring(0, 2));
-            int minute = Integer.parseInt(endTime.substring(3)) + CLEANING_MINUTE;
-            if (minute >= 60) {
-                hour++;
-                minute -= 60;
-            }
-            String strMin = minute > 10 ? String.valueOf(minute) : "0" + minute;
-            return String.format("%s:%s", hour, strMin); // 여기서 주의; 17:0 이런식으로 반환되어서 문제 발생 가능성
         }
     }
 
@@ -49,6 +39,19 @@ public class P_FindHotelRoom_F {
         }
     }
 
+    static String updateEndTime(String endTime) {
+        int hour = Integer.parseInt(endTime.substring(0, 2));
+        int minute = Integer.parseInt(endTime.substring(3)) + CLEANING_MINUTE;
+        if (minute >= 60) {
+            hour++;
+            minute -= 60;
+        }
+
+        String strHour = hour >= 10 ? String.valueOf(hour) : "0" + hour;
+        String strMin = minute >= 10 ? String.valueOf(minute) : "0" + minute;
+        return String.format("%s:%s", strHour, strMin); // 여기서 주의; 17:0 이런식으로 반환되어서 문제 발생 가능성
+    }
+
     boolean isAvail(Schedule oldData, Schedule newData) {
         // boolean startTimeAvail = newData.startTime.compareTo(oldData.startTime) < 0;
         // boolean endTimeAvail = newData.endTime.compareTo(oldData.endTime) < 0;
@@ -62,24 +65,18 @@ public class P_FindHotelRoom_F {
 
         for (String[] row : book_time) {
             Schedule newSchedule = new Schedule(row[0], row[1]);
-            Room roomPtr;
-            if (rooms.isEmpty()) {
-                roomPtr = new Room(newSchedule);
-                rooms.add(roomPtr);
-            } else {
-                boolean needMoreRoom = true;
-                for (Room room : rooms) {
-                    // 기존 스케쥴 리스트를 순회하며 해당 방이 이용가능한지 탐색 후 새로 방 추가가 필요한지 확인
-                    if (room.isAvailable(newSchedule)) {
-                        room.addSchedule(newSchedule); // ok
-                        needMoreRoom = false;
-                        break;
-                    }
+            boolean needMoreRoom = true;
+            for (Room room : rooms) {
+                // 기존 스케쥴 리스트를 순회하며 해당 방이 이용가능한지 탐색 후 새로 방 추가가 필요한지 확인
+                if (room.isAvailable(newSchedule)) {
+                    room.addSchedule(newSchedule); // ok
+                    needMoreRoom = false;
+                    break;
                 }
+            }
 
-                if (needMoreRoom) {
-                    rooms.add(new Room(newSchedule));
-                }
+            if (needMoreRoom) {
+                rooms.add(new Room(newSchedule));
             }
         }
         return rooms.size();
@@ -88,19 +85,23 @@ public class P_FindHotelRoom_F {
     // 내림차순 정렬
     void sortByTerms(String[][] book_time) {
         Arrays.sort(book_time, (b1, b2) -> {
-            int t1_start = Integer.parseInt(b1[0].substring(0, 2) + b1[0].substring(3));
-            int t1_end = Integer.parseInt(b1[1].substring(0, 2) + b1[1].substring(3));
-            int t2_start = Integer.parseInt(b2[0].substring(0, 2) + b2[0].substring(3));
-            int t2_end = Integer.parseInt(b2[1].substring(0, 2) + b2[1].substring(3));
-            return Integer.compare(Math.abs(t1_end - t1_start), Math.abs(t2_end - t2_start));
+            int result = b1[0].compareTo(b2[0]);
+            if (result == 0) {
+                String t1_end = updateEndTime(b1[1]);
+                String t2_end = updateEndTime(b2[1]);
+                return t1_end.compareTo(t2_end);
+            } else {
+                return result;
+            }
         });
     }
 
     public static void main(String[] args) {
         P_FindHotelRoom_F hotel = new P_FindHotelRoom_F();
         // String[][] book_time = {{"15:00", "17:00"}, {"16:40", "18:20"}, {"14:20", "15:20"}, {"14:10", "19:20"}, {"18:20", "21:20"}};
-        String[][] bound_test = {{"15:00", "16:50"}, {"23:00", "23:50"}};
+        String[][] bound_test = {{"00:00", "03:55"},{"00:00", "02:55"}, {"09:55", "10:55"}, {"11:05", "16:50"}, {"23:00", "23:50"}};
+        String[][] test2 = {{"23:12", "23:30"}, {"01:23", "01:54"}, {"02:04", "03:00"}};
 
-        System.out.println(hotel.solution(bound_test));
+        System.out.println(hotel.solution(test2));
     }
 }
