ee6e35968069e13194439255f9886a05795c93c8
Junho Choi <ajchoi0928@gmail.com>
2023-03-28 00:09:32 +0900

    refactor: 0의 개수 세기 O(1)
    - 0을 제거하기 전 문자열 길이 - 0 제거 후 문자열 길이

--- src/category/string/P_MultipleBinaryFlip.java
+++ src/category/string/P_MultipleBinaryFlip.java
@@ -6,8 +6,11 @@ public class P_MultipleBinaryFlip {
         int iter = 0;
         int zeroCnt = 0;
         do {
-            zeroCnt += countZeros(s);
+            int before = s.length();
             s = s.replace("0", "");
+            int after = s.length();
+            zeroCnt += before - after;
+
             s = Integer.toBinaryString(s.length());
             iter++;
         } while(s.length() > 1);
@@ -15,14 +18,5 @@ public class P_MultipleBinaryFlip {
         answer[0] = iter;
         answer[1] = zeroCnt;
         return answer;
-    }
-
-    // O(N)
-    int countZeros(String s) {
-        int result = 0;
-        for(char c : s.toCharArray()) {
-            if(c == '0') { result++; }
-        }
-        return result;
     }
 }
