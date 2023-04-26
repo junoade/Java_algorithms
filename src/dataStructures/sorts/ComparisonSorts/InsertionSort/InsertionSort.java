df4864111d6bf39b3fcdc1ab6b3e22c31737ddfd
Junho Choi <ajchoi0928@gmail.com>
2023-04-26 22:32:06 +0900

    review: 정렬 구현 연습(Insertion Sort) - 제네릭한 코드 작성

--- src/dataStructures/sorts/ComparisonSorts/InsertionSort/InsertionSort.java
+++ src/dataStructures/sorts/ComparisonSorts/InsertionSort/InsertionSort.java
@@ -1,4 +1,21 @@
 package dataStructures.sorts.ComparisonSorts.InsertionSort;
 
 public class InsertionSort {
+    public static <T extends Comparable<? super T>> void sort(T[] a) {
+        final int L = a.length;
+        for (int i = 1; i < L; i++) {
+            int j = i;
+            T keyValue = a[i];
+
+            // value를 정렬된 집합의 맨 오른쪽(j-1)과 비교해가며
+            // 대소 비교 후 오른쪽으로 값을 shift하여 자신의 삽입 위치를 찾아간다.
+            // j = 1부터 시작하므로
+            // 어떤 구현은 j = i - 1부터 시작할 수도 그러면 아래의 세부 조건이 바뀌긴 한다
+            while (j > 0 && a[j - 1].compareTo(keyValue) > 0) {
+                a[j] = a[j - 1];
+                j--;
+            }
+            a[j] = keyValue; // 찾은 삽입 위치에 삽입
+        }
+    }
 }
---
+++ src/dataStructures/sorts/ComparisonSorts/InsertionSort/Test.java
@@ -0,0 +1,29 @@
+package dataStructures.sorts.ComparisonSorts.InsertionSort;
+
+import dataStructures.sorts.ComparisonSorts.BubbleSort.MyBubbleSort;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        testIntegerArr();
+        testFloatArr();
+    }
+
+    static void testIntegerArr() {
+        Integer[] arr = {1, 99, 123, 14, 5, 5, 124, 11};
+        display(arr);
+    }
+
+    static void testFloatArr() {
+        Float[] arr = {1.0f, 99f, 123f, 14f, 5f, 5f, 124.59f, 11.12f};
+        display(arr);
+    }
+
+    private static <T extends Comparable<? super T>> void display(T[] arr) {
+        System.out.println("타입 : " + arr.getClass());
+        System.out.println("Before Sorting : " + Arrays.toString(arr));
+        MyBubbleSort.sort(arr);
+        System.out.println("After Sorting : " + Arrays.toString(arr));
+    }
+}
