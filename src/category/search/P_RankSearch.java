669b705e36acd306b90b8e6fcc074b86a81f9152
Junho Choi <ajchoi0928@gmail.com>
2023-05-12 19:04:13 +0900

    feat: 순위 검색
    I. Map<String, List<Integer>> maps 를 이용해서
    - 검색 키에 대한 점수 배열을 갖도록 구현
    
    II. 조합을 이용해서 검색 키를 구현하는 과정
    - info 배열의 요소들의 값 ex) ["java", "backend", "junior", "pizza"] 들과
    - 와일드 문자 -를 조합
     ex) "-", "backend", "junior", "pizza"
     ex) "-", "-", "junior", "pizza"
    - 검색 필터의 갯수만큼 depth 가 진행되면 중복 여부 확인하고
      점수를 넣어줌
     (주의) 검색 키들을 구분하는 구분자 넣을 때 마지막에 추가되는거 주의
    
    III. query 배열을 순회하며 각 query에 대해 문자열 분리 후 key를 생성
    - TIP : 정규표현식 " (and )?" 이용!
    - 다시 문자열로 조인해서 key 생성; CharSequence delimiter, CharSequence... elements 이용
    
    - String key, int score를 파라미터로 파라메트릭 서치 수행
    
    IV. 파라메트릭 서치
    - 점수 x점 이상 받은 사람의 수를 구하는 문제를
    - 전체 사람 수 - (점수 x 점 이상 받은 사람 중 첫번째 사람의 위치) 문제로 바꿈
     - 0부터 시작하니까 - 1 은 따로 하지 않았음
     --> 점수 x점 이상인 사람 중 가장 낮은 점수의 사람의 위치를 반환
      i) start, end 는 inclusive하게 [start, end];
      ii) 동일한 점수인 경우 제일 먼저 등장한 사람을 계속 탐색할 수 있게
      iii) 모두 [F, F]일 수 있으므로 마지막에 한번더 조건 만족 여부 판별

--- src/category/search/P_RankSearch.java
+++ src/category/search/P_RankSearch.java
@@ -1,162 +1,135 @@
 package category.search;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * --------------------------------------------------------------<br/>
  * <b>P_순위 검색</b><br/>
  * --------------------------------------------------------------<br/>
- * <b> 주요 키포인트 </b><br/>
- * <br/>
- * --------------------------------------------------------------<br/>
  * <b> 나의 풀이 </b><br/>
- * 1. 입력 처리 : 객체 배열 생성<br/>
- * 2. 객체 배열 정렬 : O(N log N), N in [1, 50,000]
- * 3. 각 쿼리를 순회 M : [1, 100,000]
- *  3-1. 쿼리 문자열로부터 조건, 점수를 분리 O(L) : 배열의 길이 만큼 순회될 수 있음
- *  3-2. 이진 탐색 수행 O(log N) 후 점수가 들어갈 자리의 idx를 찾음
- *  3-3. 해당 idx 이후부터 만족하는 객체의 개수를 O(N)으로 센다 [1, 50,000]
- *  -> 최종 시간 복잡도 O(MN) 시간초과
+ * 1. 입력 처리 : info 배열의 문자열 원소에 대해 N : 50,000<br/>
+ *  1.2 와일드 카드 '-' 를 포함한 모든 가능한 key 문자열 생성<br/>
+ *  1.3 재귀를 이용하여 구현; depth == 검색 필터의 개수일 때  // 최대 108 가지 경우<br/>
+ *  1.4 이미 존재하는 key인지 아닌지 검사 후 점수 삽입<br/>
+ *  1.5 삽입 완료 후 존재하는 키의 점수 리스트에 대해 오름차순 정렬 수행<br/>
+ * -> 전처리 시간 복잡도 O(NP + Q*Nlog N); P: 생성되는 조건의 수, 16 Q: 생성된 리스트의 개수, 108<br/>
+ *
+ * 2. 쿼리 배열을 순회 하며 maps에서 관리 중인 key인지 확인 후 M : 100,000<br/>
+ *  2.1 검색할 key 문자열, score 정수 생성<br/>
+ *  2.2 파라메트릭 서치 수행<br/>
+ * -> 최악 시간 복잡도 : O(M * log N)<br/>
+ *
+ * 3. 파라메트릭 서치 수행 O(log N);<br/>
+ *  3.1 x 점 이상 받은 사람의 수 = 전체 사람의 수 - x 점 이상인 사람 중 가장 적은 점수를 받은 사람의 위치;<br/>
+ *      0부터 시작하므로 -1 은 하지 않음<br/>
+ *  i)   start, end 범위를 inclusive하게;<br/>
+ *  ii)  반복 조건은 탐색 요소가 1개 보다 많을 때; end - start - 1 > 1 -> start < end;<br/>
+ *  iii) 동일한 점수인 경우 가장 왼쪽의 점수의 위치를 반환할 수 있게 구현<br/>
+ *  iv)  모든 경우가 [F, F] 인 경우 있을 수도 있어서 탐색 종료 후 한번 더 조건 여부 판단<br/>
  * --------------------------------------------------------------<br/>
  * <b> 채점 </b><br/>
- * <p> 정확도 13.3 점 / 효율성 0점<br/>
+ * <p> 최대 시간 1561ms / 메모리 300MB <br/>
  * --------------------------------------------------------------
  */
 public class P_RankSearch {
-    static class Person implements Comparable<Person> {
-        String language;
-        String part;
-        String experience;
-        String favorite;
-        int score;
+    // 모든 조합에 대해 점수 List 관리
+    Map<String, List<Integer>> maps;
 
-        Person(String[] inputs) {
-            if (inputs.length < 5) {
-                return;
-            }
-
-            this.language = inputs[0];
-            this.part = inputs[1];
-            this.experience = inputs[2];
-            this.favorite = inputs[3];
-            this.score = Integer.parseInt(inputs[4]);
-        }
-
-        Person(int score) {
-            this.score = score;
-        }
-
-        @Override
-        public int compareTo(Person p) {
-            return Integer.compare(this.score, p.score);
-        }
-
-        @Override
-        public String toString() {
-            return String.format("Person{%s, %s, %s, %s, %d}", language, part, experience, favorite, score);
-        }
-    }
-
-    // 인스턴스 변수
-    Person[] arr;
-
-    // N : [1, 50,000], M : [1, 100,000]
+    // info.length in [1, 50,000] / query.lenth in [1, 100,000]
     public int[] solution(String[] info, String[] query) {
-        int[] answer = new int[query.length]; // M: [1, 100,000];
+        int[] answer = new int[query.length]; // [1, 100,000];
 
         // 입력 처리
-        arr = processInput(info);
+        processInput(info);
 
-        // 입력 정렬 (점수 순으로 일단)
-        Arrays.sort(arr);
-
-        // 해당 점수 범위에 대해 이진 탐색 수행
-        // 그 후 idx 이후에 대해 값을 만족하는지 탐색
-        // O(M * N) 시간 초과
+        // query 추출 후
+        // 해당 query를 키로 갖는 리스트에 대해
+        // 정렬 후 파라메트릭 서치 수행
         int idx = 0;
-        for (String q : query) {
-            String[] inputs = processQuery(q);
-            // System.out.println(Arrays.toString(inputs));
+        for(String q : query) {
+            String[] temp = q.split(" (and )?");
+            int L = temp.length;
+            String key = String.join(" ", Arrays.copyOf(temp, L - 1));
+            int score = Integer.parseInt(temp[L - 1]);
+            // System.out.println(key);
+            answer[idx] = search(key, score);
 
-            // 조건에 부합하는지 판별
-            answer[idx++] = getCount(inputs);
+            idx++;
         }
+
 
         return answer;
     }
 
-    // 입력 처리
-    Person[] processInput(String[] info) {
-        Person[] arr = new Person[info.length];
-
-        int idx = 0;
-        for (String s : info) {
+    // 입력에 대해 분리 후
+    // 모든 조합 생성 및 점수 리스트 구성
+    // 정렬도 미리 수행
+    void processInput(String[] info) {
+        maps = new HashMap<>(108); // 가지수
+        for(String s : info) {
             String[] inputs = s.split(" ");
-            arr[idx++] = new Person(inputs);
+            rec("", inputs, 0);
         }
 
-        // 확인
-        // System.out.println(Arrays.toString(arr));
-        return arr;
+        for(List<Integer> list : maps.values()) {
+            Collections.sort(list);
+        }
     }
 
-    // query 처리
-    String[] processQuery(String query) {
-        String[] inputs = query.split(" |and");
-        List<String> list = new ArrayList<>();
-        for (String s : inputs) {
-            if (!s.equals("")) {
-                list.add(s);
+    void rec(String s, String[] inputs, int depth) {
+        // base case;
+        if(depth == inputs.length - 1) { // 점수를 제외한 부분 까지만
+            int score = Integer.parseInt(inputs[inputs.length - 1]);
+            s = s.trim();
+            List<Integer> temp;
+            if(maps.containsKey(s)) {
+                temp = maps.get(s);
+            } else {
+                temp = new ArrayList<>();
+            }
+            temp.add(score);
+            maps.put(s, temp);
+
+            return;
+        }
+
+        rec(s + inputs[depth] + " ", inputs, depth + 1);
+        rec(s + "-" + " ", inputs, depth + 1);
+    }
+
+    // 파라메트릭 서치 수행
+    // 조건을 만족하는 사람 중 코딩테스트 점수를 x점 이상 받은 사람은 모두 몇 명인가? 를 구해야하는데
+    // 1) 모든 사람들의 수
+    // 2) x 점 이상인 사람들 중 가장 낮은 점수의 위치를 뺀다
+    int search(String key, int target) {
+        if(!maps.containsKey(key)) {
+            return 0;
+        }
+        List<Integer> list = maps.get(key);
+
+        // Collections.sort(list); // 매번 정렬하는 것보단 처음 한번에
+
+        int start = 0; // inclusive
+        int end = list.size() - 1; // inclusive
+
+        while(start < end) {
+            int mid = (start + end) / 2;
+            int midValue = list.get(mid);
+
+            if(midValue < target) { // 조건 X
+                start = mid + 1;
+            } else { // 조건 O // midValue >= target
+                end = mid;
             }
         }
 
-        return list.stream().toArray(String[]::new);
-    }
-
-    // binarySearch 수행 후
-    // idx로 부터 끝까지 탐색 O(log N + N) = O(N)
-    int getCount(String[] inputs) {
-        int start = 0;
-        int end = arr.length;
-        int score = Integer.parseInt(inputs[4]);
-        int count = 0;
-
-        // binarySearch(Object[] a, Object key)
-        int idx = Arrays.binarySearch(arr, new Person(score));
-        if (idx < 0) {
-            idx = Math.abs(idx) - 1;
+        int resultIdx = start;
+        // 모든 경우가 [F, F] 인 경우일 수 있음
+        if(list.get(resultIdx) < target) {
+            return 0;
+        } else {
+            return list.size() - resultIdx;
         }
-
-        for (; idx < end; idx++) {
-            if (checkCond(arr[idx], inputs)) {
-                count++;
-            }
-        }
-
-        return count;
-    }
-
-    boolean checkCond(Person p, String[] inputs) {
-        // 서로 일치 하지 않거나(and) "-"이지도 않은 경우
-        if (!inputs[0].equals(p.language) && !inputs[0].equals("-")) {
-            return false;
-        }
-
-        if ( !inputs[1].equals(p.part) && !inputs[1].equals("-")) {
-            return false;
-        }
-
-        if (!inputs[2].equals(p.experience) && !inputs[2].equals("-")) {
-            return false;
-        }
-
-        if (!inputs[3].equals(p.favorite) && !inputs[3].equals("-")) {
-            return false;
-        }
-
-        return Integer.parseInt(inputs[4]) <= p.score;
     }
 
     public static void main(String[] args) {
