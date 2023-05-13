cc4e1578578a4c6f2a3e75046c1f371c7c7905ce
Junho Choi <ajchoi0928@gmail.com>
2023-05-13 18:17:18 +0900

    feat: 징검다리
    ### 알고리즘 스케치 - 결정 조건 구현 요약
    I.. 지점 사이의 최소 값 d가 가능한 범위를 고려
    II. **결정 조건; O(M)**
        1. 지점 사이의 최소 값이 d라고 가정했을 때
        2. O(M) 시간 복잡도로 rocks 를 순회하며 현재 지점 - 이전 지점 의 actual 한 거리와 d를 비교하자
            1. 만약 실제 거리 차이가 가정한 최소 거리보다 작다면
            2. 현재 위치의 돌을 제거해서 가정을 맞추도록 한다고 생각하자
        3. 제거한 돌의 갯수와 n를 비교해서 결정 조건의 참/거짓을 반환하자
    III. 반복 범위 관련
    IV. 파라메트릭 서치 범위 관련

--- src/category/search/P_RocksOnBridge.java
+++ src/category/search/P_RocksOnBridge.java
@@ -1,66 +1,98 @@
 package category.search;
 
+import java.util.Arrays;
+
 /**
  * --------------------------------------------------------------<br/>
  * <b>P_징검다리</b><br/>
  * --------------------------------------------------------------<br/>
  * <b> 주요 키포인트 </b><br/>
- * <br/>
+ * 결정 조건을 어떻게 정의할 것인가?<br/>
  * --------------------------------------------------------------<br/>
  * <b> 나의 풀이 </b><br/>
- * 1. 바위 배열 M을 순회하면서; M in [1, 50,000] <br/>
- *  - 바위 i, 바위 j를 거리 비교할 수 있도록 [1, 50,000] 에 대해 또 순회 O(M^2) = 25억
- *  - 바위 위치에서 시작점 , 끝점 과 거리 비교 (아무런 장애물 없다고 가정)
- *  - 바위 i에서 다음 바위 j와의 거리 비교
- *  - min distance 값 갱신
- *  2. 해당 최솟값 중에 최대를 반환
+ * 1. 지점 사이의 최소 값 d가 가능한 범위를 고려하자<br/>
+ * 2. 결정 조건<br/>
+ *  - 지점 사이의 최소 값이 d라고 가정했을 때<br/>
+ *  - O(M) 시간 복잡도로 rocks 를 순회하며 현재 지점 - 이전 지점 의 actual 한 거리와 d를 비교하자<br/>
+ *  - 만약 실제 거리 차이가 가정한 최소 거리보다 작다면<br/>
+ *  - 현재 위치의 돌을 제거해서 가정을 맞추도록 한다고 생각하자<br/>
+ *  - 제거한 돌의 갯수와 n를 비교해서 결정 조건의 참/거짓을 반환하자<br/>
+ *
+ *  3. 반복 범위 관련<br/>
+ *  - 조건을 만족하는 요소 중 최대 요소를 찾아야 하므로 [start, end) 범위를 두어 end 포인터가 끝 + 1 를 가르키도록 하자;<br/>
+ *  - 반복은 탐색 요소의 개수가 1이 될 때까지 반복한다.<br/>
+ *
+ *  4. 파라메트릭 서치<br/>
+ *  - 가정한 최솟값 d에 대해 결정 조건이 참이라면 더 큰 범위에 대해 탐색할 수 있도록 범위를 조절한다.<br/>
+ *     - 동일한 경우에도 더 뒤에 위치 가능한 참인 결정 조건 을 탐색할 수 있도록 한다.<br/>
+ *  - 가정한 최솟값 d에 대해 결정 조건이 거짓이라면 더 작은 범위에 대해 탐색할 수 있도록 범위를 조절한다.<br/>
+ *     - end = d; 로 하면 end 는 탐색 범위의 exclusive한 위치에 존재하게 된다.<br/>
+ *  - d가 갖을 수 있는 모든 경우에 대해 이분 탐색을 수행하므로 [F, F] 인 경우는 존재하지 않는다.<br/>
+ *
+ *  5. 기타<br/>
+ *  - n = rocks.length; 일 때 이 떄 d는 마지막 위치 - 초기 위치 = distance가 d가 될 것이다.<br/>
+ *  - 이를 rocks의 마지막에 넣어주기 위해, 배열 길이 L + 1을 갖는 배열로 copy하고 마지막 요소 위치에 추가해준다.<br/>
  * --------------------------------------------------------------<br/>
  * <b> 채점 : 22.2점 </b><br/>
- * <p> 메모리  200MB, 실행시간 : 시간 초과<br/>
+ * <p> 메모리  100MB, 실행시간 : 26ms <br/>
  * --------------------------------------------------------------
  */
 public class P_RocksOnBridge {
-
     public int solution(int distance, int[] rocks, int n) {
-        int answer = 0;
 
-        // O(M^2)
-        long start = 1;
-        long end = n;
+        // 답해서 구하고자 하는 distance의 가능한 범위;
+        // 이 범위안에 답은 반드시 하나 포함된다.
+        // 조건을 만족하는 범위 중 최대값을 찾는 경우이므로 [start, end)
+        int start = 1;
+        int end = distance + 1;
 
+        // 만약 n = rocks.length 인 경우
+        // 이 때 구하고자 하는 d는 end - start 일 것이다.
+        // 이를 포함해서 검사하도록 하자
         final int L = rocks.length;
-        int totalMax = -1;
+        rocks = Arrays.copyOf(rocks, L + 1); // int[] original, int newLength;
+        rocks[L] = distance;
+        Arrays.sort(rocks);
 
+        while(end - start > 1) {
+            int d = (start + end) / 2; // 각 지점 사이의 최솟값을 d라고 가정
 
-        for(int i = 0; i < L; i++) {
-            long[] temp = new long[L + 1];
-            long min = Long.MAX_VALUE;
+            boolean cond = isPossible(d, n, rocks); // 결정 조건을 검사
 
-            for(int j = 0; j < L; j++) {
-                if(i == j) {
-                    temp[0] = Math.abs(rocks[i] - start); // 시작점에서 해당 지점까지 거리
-                    temp[L - 1] = Math.abs(end - rocks[i]); // 끝점에서 해당 지점까지 거리
-                    if(min > temp[0]) {
-                        min = temp[0];
-                    }
-
-                    if(min > temp[L - 1]) {
-                        min = temp[L - 1];
-                    }
-
-                    continue;
-                }
-
-                temp[j] = Math.abs(rocks[j] - rocks[i]);
-                if(min > temp[j]) {
-                    min = temp[j];
-                }
-            }
-
-            if(min > totalMax) {
-                totalMax = (int)min;
+            if(cond) {
+                // 더 큰 d를 가정하고 검사하도록
+                start = d;
+            } else {
+                // 더 작은 d를 가정하고 검사할 수 있도록
+                end = d;
             }
         }
-        return totalMax;
+
+        // [F, F] 인 경우는 존재하지 않으므로 별도의 검사가 필요하지 않다
+
+        return start;
+    }
+
+    // 결정 조건
+    // 각 지점 사이의 최솟값을 d라고 고정했다고 가정하자.
+    // 현재 위치 - 이전 위치의 거리(acutal)가 < d 라면 앞선 가정에 맞지 않으므로
+    // 현재 위치의 돌을 제거한다고 고려하자 removeCount++
+    // 최종 removeCnt 와 n을 비교해서 n개 이하를 제거했을 때 최소 거리 d를 얻을 수 있는지 결정한다.
+    boolean isPossible(int d, int n, int[] rocks) {
+        // O(M)
+        int removeCnt = 0;
+        int last = 1;
+
+        for(int rock : rocks) {
+            int actual = rock - last;
+            if(actual < d) {
+                removeCnt++;
+                continue;
+            }
+
+            last = rock;
+        }
+
+        return removeCnt <= n;
     }
 }
