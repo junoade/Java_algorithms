dca4f1ef2f0c08a6b0f5f5eb2427a994527b731d
Junho Choi <ajchoi0928@gmail.com>
2023-05-29 21:07:04 +0900

    feat : B_잃어버린 괄호_1541
    괄호의 의미와 괄호를 어디다가 쳐야 최소가 되는가?에 대해 다시 고민
    a - b + c - d - e - f 라면
    a - (b+c) - d - e - f 가 된다면 최소값을 반환하게 될 것
    
    -> '-' 연산자를 기준으로 문자열을 분리하고 (괄호를 치고)
    -> 문자열 배열에 대해 + 로 문자열 구분후 값을 더한 다음
    -> 첫 문자열에 대한 값 이후 순서부터 뺄셈 처리해주기

--- src/category/bruteforce/B_LostParenthesis_1541.java
+++ src/category/bruteforce/B_LostParenthesis_1541.java
@@ -3,61 +3,32 @@ package category.bruteforce;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class B_LostParenthesis_1541 {
 
-    static final String REGEX = "[+|-]";
-    static int MIN;
-    static boolean[] visited;
-
     static void solution(String s) {
-        int[] numbers = Arrays.stream(s.split(REGEX)).mapToInt(Integer::parseInt).toArray();
-        String[] ops = Arrays.stream(s.split("(\\d)+")).skip(1).toArray(String[]::new);
-        // System.out.println(Arrays.toString(numbers));
-        // System.out.println(Arrays.toString(ops));
+        String[] exprs = s.split("-");
 
-        MIN = Integer.MAX_VALUE;
-        visited = new boolean[ops.length];
+        int sum = 0;
+        for(int i = 0; i < exprs.length; i++) {
+            int num = 0;
 
-        rec(numbers, ops, 0, 0);
-        System.out.println(MIN);
-    }
-
-    static void rec(int[] numbers, String[] ops, int depth, int value) {
-        // base case;
-        if (depth == ops.length) {
-            MIN = Math.min(MIN, value);
-            System.out.println("EXIT");
-            return;
-        }
-
-        for (int i = 0; i < ops.length; i++) {
-            if (visited[i]) {
-                continue;
+            if(exprs[i].length() != 1) {
+                for(String digit : exprs[i].split("\\+")) {
+                    num += Integer.parseInt(digit);
+                }
+            } else {
+                num = Integer.parseInt(exprs[i]);
             }
 
-            visited[i] = true;
-            if (depth == 0) {
-                value = numbers[i];
-            }
-
-            if (ops[i].equals("+")) {
-                System.out.printf("expr{%d %s %d = ", value, "+", numbers[i + 1]);
-                value = value + numbers[i + 1];
-                System.out.printf("%d}\n", value);
-                rec(numbers, ops, depth + 1, value);
-                visited[i] = false;
-                value -= numbers[i + 1];
-            } else if (ops[i].equals("-")) {
-                System.out.printf("expr{%d %s %d = ", value, "-", numbers[i + 1]);
-                value = value - numbers[i + 1];
-                System.out.printf("%d}\n", value);
-                rec(numbers, ops, depth + 1, value);
-                visited[i] = false;
-                value += numbers[i + 1];
+            if(i == 0) {
+                sum = num;
+            } else {
+                sum -= num;
             }
         }
+
+        System.out.println(sum);
     }
 
     public static void main(String[] args) throws IOException {
