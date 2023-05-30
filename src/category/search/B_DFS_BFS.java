857605cb71a91e4c5e6e80d6272fe99a35305490
Junho Choi <ajchoi0928@gmail.com>
2023-05-30 21:09:34 +0900

    feat: B_DFS와 BFS_1260
    인접행렬을 이용하여 그래프 탐색 과정 시 방문한 정점 출력
    DFS
    BFS

---
+++ src/category/search/B_DFS_BFS.java
@@ -0,0 +1,84 @@
+package category.search;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class B_DFS_BFS {
+    static int[][] graph;
+    static boolean[] visited;
+
+    static void dfs(int startKey, StringBuilder sb) {
+        visited[startKey] = true;
+
+        for(int i = 1; i < graph[startKey].length; i++) {
+            if(visited[i]) {
+                continue;
+            }
+
+            if(graph[startKey][i] != 1) {
+                continue;
+            }
+
+            visited[i] = true;
+            sb.append(i).append(" ");
+            dfs(i, sb);
+        }
+    }
+
+    static void bfs(int startKey, StringBuilder sb) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(startKey);
+        visited[startKey] = true;
+
+        while (!queue.isEmpty()) {
+            Integer cursor = queue.poll();
+
+            for(int i = 1; i < graph[cursor].length; i++) {
+                if(visited[i]) {
+                    continue;
+                }
+
+                if(graph[cursor][i] != 1) {
+                    continue;
+                }
+
+                visited[i] = true;
+                sb.append(i).append(" ");
+                queue.add(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        // 메모리 공간 O(N^2)
+        graph = new int[N+1][N+1];
+        for(int i = 0; i < M; i++) {
+            int[] inputs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int src = inputs[0], target = inputs[1];
+            // 양방향이라
+            graph[src][target] = 1;
+            graph[target][src] = 1;
+        }
+
+        visited = new boolean[N + 1]; // 정점은 1부터 N까지
+        StringBuilder result = new StringBuilder(V + " ");
+        dfs(V, result);
+        System.out.println(result);
+
+        visited = new boolean[N + 1]; // 정점은 1부터 N까지
+        result = new StringBuilder(V + " ");
+        bfs(V, result);
+        System.out.println(result);
+    }
+}
