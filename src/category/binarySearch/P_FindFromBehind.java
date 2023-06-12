90b4aa3d998e224bd5f5bb2dc779c33f27df1d17
Junho Choi <ajchoi0928@gmail.com>
2023-06-12 21:58:54 +0900

    failed: P_뒤에 있는 큰 수 찾기 (30.4 / 100)
    Q2. 정렬을 안해보고 모든 위치에 대해 탐색해봤다...
    시간초과 문제가 있었다!

--- src/category/binarySearch/P_FindFromBehind.java
+++ src/category/binarySearch/P_FindFromBehind.java
@@ -12,8 +12,9 @@ import java.util.Arrays;
  * <b> 나의 풀이 </b><br/>
  * 이진 탐색 - 파라메트릭 서치; 조건을 만족하는 수 중 가장 작은 위치를 찾는 방법을 고민해봄<br/>
  * Q1. 이진 탐색의 기본은 정렬 후 O(log N) 으로 탐색하는 건데, numbers 배열은 정렬하면 안될 것 같다. 어떡하지?<br/>
+ * Q2. 정렬을 안해보고 왼쪽, 오른쪽의 값들을 모두 찾아 반환해봤다! 그런데 시간 초과 문제가 있다.
  * --------------------------------------------------------------<br/>
- * <b> 채점 26.1 / 100</b><br/>
+ * <b> 채점 30.4 / 100</b><br/>
  * --------------------------------------------------------------
  */
 public class P_FindFromBehind {
@@ -32,35 +33,48 @@ public class P_FindFromBehind {
     }
 
     int binarySearch(int[] numbers, int key, int start, int to) {
-        while(to - start > 1) {
-            int mid = (start + to) / 2 ;
-            boolean cond = isValid(key, numbers[mid]);
 
-            if(cond) {
-                to = mid;
-            } else {
-                start = mid;
-            }
-        }
-
+        // base case #1
         if(start > to) {
             return -1;
-        } else if (start == to) {
+        }
+
+        // base case #2 : 1개만 남았을 때
+        if (start == to) {
             return isValid(key, numbers[start]) ? numbers[start] : -1;
-        } else {
+        }
+
+        // base case #3 : 2개만 남았을 때
+        // 조건을 만족하는 min을 반환
+        if (to - start == 1) {
             int lastLeft = numbers[start];
             int lastRight = numbers[to];
-            if(!isValid(key, lastLeft)) {
-                if(!isValid(key, lastRight)) {
-                    return -1;
-                } else {
-                    return lastRight;
-                }
-            } else {
+            if(!isValid(key, lastLeft) && !isValid(key, lastRight)) {
+                return -1;
+            }
+
+            if(isValid(key, lastLeft)) {
                 return lastLeft;
+            }
+
+            if(isValid(key, lastRight)) {
+                return lastRight;
             }
         }
 
+        // general case
+        int target1 = 0, target2 = 0;
+        int mid = (start + to) / 2 ;
+        boolean cond = isValid(key, numbers[mid]);
+        target1 = binarySearch(numbers, key, start, mid);
+        target2 = binarySearch(numbers, key, mid, to);
+        if(target1 == -1 && target2 == -1) {
+            return -1;
+        }
+        if(target1 != -1) {
+            return target1;
+        }
+        return target2;
     }
 
     boolean isValid(int n1, int n2) {
