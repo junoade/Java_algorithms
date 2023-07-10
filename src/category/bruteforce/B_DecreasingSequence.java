411d37ebc80bd3708c8ac67976ea81aeb18f6357
Junho Choi <ajchoi0928@gmail.com>
2023-07-11 07:07:29 +0900

    feat: 백준 줄어드는 수 1174
    P_모음사전 처럼
    
    주어진 조건에 맞는 최대 수는 9876543210이였다.
    이를 바탕으로 int[] symbols = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
    를 선언한 다음,
    r in [1, N] 범위에 대해 순열을 백트랙킹 방식으로 구현해준다.

--- src/category/bruteforce/B_DecreasingSequence.java
+++ src/category/bruteforce/B_DecreasingSequence.java
@@ -4,53 +4,65 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class B_DecreasingSequence {
-    static final int N = 1_000_000;
 
-    //
-    static int solution(int idx) {
-        List<Integer> list = makeList();
+    static List<Long> list;
+    static final int[] symbols = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    static boolean[] visited;
 
-        if (idx >= list.size() || idx <= 0) {
+    static long solution(int N) {
+
+        list = new ArrayList<>();
+        final int L = symbols.length;
+        visited = new boolean[L];
+
+        // 백트랙킹으로 가능한 모든 감소하는 수의 순열을 구함
+        for (int r = 1; r <= L; r++) {
+            recursive(L, r, 0, new StringBuilder());
+        }
+
+        Collections.sort(list);
+
+        if (list.size() < N) {
             return -1;
         }
 
-        return list.get(idx - 1);
+        return list.get(N - 1);
     }
 
-    static List<Integer> makeList() {
-        List<Integer> list = new ArrayList<>();
+    static void recursive(int n, int r, int depth, StringBuilder sb) {
+        // base case : r == depth일 때
+        if (depth == r) {
+            // 추가함
+            list.add(Long.parseLong(sb.toString()));
+            return;
+        }
 
-        for (int n = 0; n <= N; n++) {
-            if (isValid(n)) {
-                list.add(n);
+        // general case
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) {
+                continue;
             }
-        }
-        return list;
-    }
 
-    static boolean isValid(int n) {
-        char[] arr = String.valueOf(n).toCharArray();
-        if (arr.length == 1) {
-            return true;
-        }
-
-        for (int i = 0; i < arr.length - 1; i++) {
-            int prev = arr[i] - '0', next = arr[i + 1] - '0';
-            // 주어진 조건과 다를 때
-            if (prev <= next) {
-                return false;
+            if (sb.length() >= 1 && sb.charAt(sb.length() - 1) - '0' < symbols[i]) {
+                continue;
             }
+
+            sb.append(symbols[i]);
+            visited[i] = true;
+            recursive(n, r, depth + 1, sb);
+            visited[i] = false;
+            sb.deleteCharAt(sb.length() - 1);
         }
 
-        return true;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int idx = Integer.parseInt(br.readLine());
-        System.out.println(solution(idx));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(solution(N));
     }
 }
