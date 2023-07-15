cba40b731e14178a86a3356f68d921af6b14e808
ajchoi0928 <ajchoi0928@gmail.com>
2023-07-15 22:42:49 +0900

    feat: B_1213_팰린드롬 만들기
    주어진 문자열의 문자 symbols들을 바탕으로
    한 문자의 등장 갯수가 짝수개, 홀수개 일 때에 대해
    경우를 나눴다.
    
    그 다음 팰린드롬의 특성에 맞춰 아래의 케이스를 구현한다.
    
    (팰린드롬은 일종의 거울로, 거꾸로 봐도 동일한 수여야 하기 때문에, 짝수개 문자는 항상 괜찮고 홀수개 문자는 딱 한 문자만 있어야한다)

--- src/category/string/B_Palindrome_1213.java
+++ src/category/string/B_Palindrome_1213.java
@@ -1,81 +1,91 @@
 package category.string;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
 
 public class B_Palindrome_1213 {
 
     // 중복 방지용
-    static Set<String> set;
     static int L;
-    static boolean[] visited;
+    static int[] records;
 
     static final String sorryWord = "I'm Sorry Hansoo";
 
     static String solution(String input) {
-        set = new HashSet<>();
         L = input.length();
 
         char[] arr = input.toCharArray();
-        visited = new boolean[L];
+        records = new int[26];
 
-        rec(arr, 0, new StringBuilder());
-
-        // 정렬 후 제일 앞에 있는 것 반환
-        // 없는 경우엔 다른 문자열 반환
-
-        String[] answers = set.toArray(String[]::new);
-        Arrays.sort(answers);
-
-        if (answers.length == 0) {
+        if (!isValid(arr)) {
             return sorryWord;
         } else {
-            return answers[0];
+            return getPalindrome();
         }
     }
 
-    // 순열 구현
-    static void rec(char[] arr, int depth, StringBuilder sb) {
-        // base case
-        // then, isPalindrome, add
-        if (depth == L) {
-            if (isPalindrome(sb.toString())) {
-                set.add(sb.toString());
-            }
-            return;
+    // 모든 문자는 대문자
+    // O(N), N = 50
+    static boolean isValid(char[] a) {
+        for (char c : a) {
+            int idx = c - 'A';
+            records[idx]++;
         }
 
-        for (int i = 0; i < L; i++) {
-            if (visited[i]) {
-                continue;
+        // 모든 문자의 개수가 짝수개 거나
+        // 모든 문자의 개수 중 홀수가 하나 이거나
+        // 짝수개의 문자는 항상 괜찮으므로 별도 체크 X
+        int oddCount = 0;
+        for (int value : records) {
+
+            // 홀수개 문자는 하나만!
+            // 그외엔 짝수개 문자여야함
+            if (value % 2 == 1) {
+                oddCount++;
             }
-            visited[i] = true;
-            sb.append(arr[i]);
 
-            rec(arr, depth + 1, sb);
-
-            visited[i] = false;
-            sb.deleteCharAt(sb.length() - 1);
-        }
-
-    }
-
-    static boolean isPalindrome(String input) {
-        final int length = input.length();
-        for (int i = 0; i < length / 2; i++) {
-            char left = input.charAt(i);
-            char right = input.charAt(length - i - 1);
-            if (left != right) {
+            if (oddCount > 1) {
                 return false;
             }
+
+
         }
         return true;
     }
 
+    static String getPalindrome() {
+        // 짝수개 문자
+        // 짝수개 문자 + 홀수개 문자 1 + 짝수개 문자
+        // 사전 순으로 가장 맨 앞에서 부터
+        StringBuilder left = new StringBuilder();
+        int oddLastIdx = -1;
 
+        for (int i = 0; i < records.length; i++) {
+            if (records[i] == 0) {
+                continue;
+            }
+            char symbol = (char) ('A' + i);
+
+            // 짝수개 반만큼 추가해줌
+            int half = records[i] / 2;
+            boolean isOdd = records[i] % 2 == 1;
+            for (; half > 0; half--) {
+                left.append(symbol);
+                records[i]--;
+            }
+
+            // 홀수개 문자인 경우
+            if (isOdd) {
+                oddLastIdx = i;
+            }
+
+        }
+
+        if (oddLastIdx == -1) {
+            return left.toString() + left.reverse().toString();
+        } else {
+            return left.toString() + (char) ('A' + oddLastIdx) + left.reverse().toString();
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         // System.setIn(new FileInputStream(""));
