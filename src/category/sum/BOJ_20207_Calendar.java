4a7f6e6a03c29345150bcf7a91cd087dd99d4980
ajchoi0928 <ajchoi0928@gmail.com>
2023-08-02 20:13:46 +0900

    feat: BOJ_20207_달력
    1
    1 365 테캐 놓침
    내 로직대로라면 1~365 검사를 위해 0번, 366번 위치가 존재해야함

--- src/category/sum/BOJ_20207_Calendar.java
+++ src/category/sum/BOJ_20207_Calendar.java
@@ -19,7 +19,7 @@ public class BOJ_20207_Calendar {
         int total = 0;
         int leftIdx = 0, rightIdx = 0;
         int prev = 0, height = 0;
-        for(int i = 1; i <= L; i++) {
+        for(int i = 1; i < L + 2; i++) {
 
             if(prev == 0 && records[i] != 0) {
                 leftIdx = i;
@@ -44,7 +44,7 @@ public class BOJ_20207_Calendar {
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        records = new int[L + 1]; // 1'based indexing
+        records = new int[L + 2]; // 1'based indexing
 
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
