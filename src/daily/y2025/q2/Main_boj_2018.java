eecac963ad4a9f4bfb9e68a76bb307d2345f4920
junoade <ajchoi0928@gmail.com>
2025-05-19 23:53:04 +0900

    refactor: BOJ 2018 수들의합5
    투포인터 풀이

--- src/daily/y2025/q2/Main_boj_2018.java
+++ src/daily/y2025/q2/Main_boj_2018.java
@@ -3,31 +3,27 @@ package daily.y2025.q2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main_boj_2018 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int start = 1;
-        int answer = 0;
-        StringBuilder sb;
-        for (; start < N; start++) {
-            int sum = start;
+        int start = 1, end = 1, sum = 1, answer = 0;
 
-            for (int end = start + 1; end < N; end++) {
-                sum += end;
-                if (N == sum) {
-                    answer++;
-
-                    break;
-                } else if (N < sum) {
-                    break;
-                }
+        while (end <= sum) {
+            if (sum < N) {
+                end++;
+                sum += end; // end 포인터 이동
+            } else if (sum > N) {
+                sum -= start; // start 포인터를 이동
+                start++;
+            } else {
+                answer++;
+                sum -= start; // start 포인터 이동
+                start++;
             }
         }
-        answer++;
         System.out.println(answer);
     }
 }
