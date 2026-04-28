29d90ebcc1bc7b487b73c350f4c355a3b828705d
junoade <ajchoi0928@gmail.com>
2026-04-28 22:06:02 +0900

    fix: Leetcode75 MergeStringAlternately
    Math.min() 을 활용한 가독성 개선

--- src/daily/y2026/LeetMergeStrings_1768.java
+++ src/daily/y2026/LeetMergeStrings_1768.java
@@ -3,7 +3,7 @@ package daily.y2026;
 /**
  * https://leetcode.com/problems/merge-strings-alternately/?envType=study-plan-v2&envId=leetcode-75
  */
-public class Leet_MergeStrings_1768 {
+public class LeetMergeStrings_1768 {
 
     public String mergeAlternately(String word1, String word2) {
 
@@ -12,17 +12,14 @@ public class Leet_MergeStrings_1768 {
         char[] wordArr2 = word2.toCharArray();
         int N = wordArr1.length, M = wordArr2.length;
 
-        for (int i = 0; i < N; i++) {
-            sb.append(wordArr1[i]);
-            if (i < M) {
-                sb.append(wordArr2[i]);
-            }
+        int minLen = Math.min(N, M);
+        for (int i = 0; i < minLen; i++) {
+            sb.append(wordArr1[i]).append(wordArr2[i]);
         }
 
         String answer = sb.toString();
-        if (N < M) {
-            answer += word2.substring(N);
-        }
+        if(N > M) answer += word1.substring(M);
+        else if(N < M) answer += word2.substring(N);
 
         return answer;
     }
