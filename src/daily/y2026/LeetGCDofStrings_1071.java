965cce3c1f585291cf0ed3f05d052423e06317cd
junoade <ajchoi0928@gmail.com>
2026-04-29 20:44:50 +0900

    refactor(string): Leetcode75 문자열 GCD
    유클리드 호제법을 통한 gcd() 계산

--- src/daily/y2026/LeetGCDofStrings_1071.java
+++ src/daily/y2026/LeetGCDofStrings_1071.java
@@ -2,50 +2,26 @@ package daily.y2026;
 
 public class LeetGCDofStrings_1071 {
     public String solution(String str1, String str2) {
-        int N = Math.min(str1.length(), str2.length());
+        String x = str1 + str2;
+        String y = str2 + str1;
 
         String answer = "";
-        for(int i = 1; i <= N; i++) {
-            String t = str1.substring(0, i);
-
-            // 부분 문자열로 나눌 수 있는 지 확인
-            if(!dividable(str1, t) || !dividable(str2, t)) {
-                continue;
-            }
-
-            // str1, str2 각각 prefix의 합으로만 이루어 져있는지 확인
-            // 그러면 임시로 저장.
-            if(hasAllCommon(str1, t) && hasAllCommon(str2, t)) {
-                answer = t;
-            }
+        if(!x.equals(y)) {
+            return answer;
         }
+
+        int gcd = gcd(str1.length(), str2.length());
+        answer = str1.substring(0, gcd);
+
         return answer;
     }
 
-    private boolean dividable(String s, String t) {
-        return s.length() % t.length() == 0;
+    private int gcd(int a, int b) {
+        // a % b == 0 이 true일때 b가 최대공약수
+        // a % b = r 이면, gcd(b, r) 는 gcd(a, b)의 최대공약수와 같다. (유클리드 호제법)
+        return b == 0 ? a : gcd(b, a % b);
     }
 
-    private boolean hasAllCommon(String s, String t) {
-        int startIdx = 0;
-        int next = t.length();
-
-        boolean result = true;
-        while(true) {
-            if(startIdx + next > s.length()) {
-                break;
-            }
-
-            String temp = s.substring(startIdx, startIdx + next);
-            if(!temp.equals(t)) {
-                result = false;
-                break;
-            }
-            startIdx += next;
-
-        }
-        return result;
-    }
 
     public static void main(String[] args) {
         LeetGCDofStrings_1071 solution = new LeetGCDofStrings_1071();
